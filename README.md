32ebbb755c960ba7eaa2edc3cff21a25964c02d7
Naethra <naethratechpvtltd@gmail.com>
2017-12-20 17:16:25 +0530

    Chat Change
    Commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# Ionic Messenger Starter
+ <img src="https://s3.amazonaws.com/ionic-marketplace/ionic-messenger-starter/icon.png" width="124px" />
+ 
+ _A simple [Ionic Messenger app](https://market.ionic.io/starters/ionic-messenger-starter) with Angularfire2 and Firebase v4._ 
+ 
+ Live at: https://ionic-messenger-starter.firebaseapp.com
+
+## Getting Started
+
+(_Please update firebase api key with yours before running._)
+
+* Clone this repository: `git clone https://github.com/sdey0081/ionic-messenger-starter.git`.
+* Run `npm install` from the project root.
+* Install the ionic CLI (`npm install -g ionic`)
+* Run `ionic serve` in a terminal from the project root.
+
+**Note:** Is your build slow? Update `npm` to 3.x: `npm install -g npm`.
+
+## Upcomming
+
+- Person to person & Group chatting feature(done).
+- FCM Notification with Firebase Function, [example](https://github.com/firebase/functions-samples/tree/master/fcm-notifications).
+- Sending photos & generate thumbnails, [example](https://github.com/firebase/functions-samples/tree/master/generate-thumbnail).
+- Available to edit old messages.
+- Sending emoticons(like Slack).
+- Etc.
+
+(_**I am still busy with multiple projects, but will try my best. Any contributors are welcome.**_)
+
+
+## Environment
+```
+Ionic CLI: 3.15.2 or greater
+Ionic Framework: ^3.7.1
+Angularfire2: ^5.0.0-rc.3
+Firebase: ^4.6.0
+Cordova CLI: ^7.0.1
+```
---
+++ src/app/app.module.ts
@@ -0,0 +1,60 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
+import { MyApp } from './app.component';
+import { ContactCardComponent } from '../components/contact-card/contact-card';
+import { ImagePickerComponent } from '../components/image-picker/image-picker';
+import { LoaderComponent } from '../components/loader/loader';
+import { AutoFocusDirective } from '../directives/auto-focus/auto-focus';
+
+import { firebaseConfig } from './app.constants';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { Facebook } from '@ionic-native/facebook';
+import { GooglePlus } from '@ionic-native/google-plus';
+import { Keyboard } from '@ionic-native/keyboard';
+
+import { AuthProvider } from '../providers/auth/auth';
+import { ChatsProvider } from '../providers/chats/chats';
+import { ContactsProvider } from '../providers/contacts/contacts';
+import { FbRtdbProvider } from '../providers/fb-rtdb/fb-rtdb';
+
+
+@NgModule({
+  declarations: [
+    MyApp,
+    ContactCardComponent,
+    ImagePickerComponent,
+    LoaderComponent,
+    AutoFocusDirective,
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule,
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    Facebook,
+    GooglePlus,
+    Keyboard,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AuthProvider,
+    ChatsProvider,
+    ContactsProvider,
+    FbRtdbProvider
+  ]
+})
+export class AppModule {}
